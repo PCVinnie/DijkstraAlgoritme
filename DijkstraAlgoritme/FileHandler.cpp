be2ddef2bc7d92a2dbce1b9f8c33165d8460b913
PCVinnie <vastout@vasnet.nl>
2015-11-25 21:42:18 +0100

    Added comments to file IO algorithm

--- DijkstraAlgoritme/FileHandler.cpp
+++ DijkstraAlgoritme/FileHandler.cpp
@@ -91,12 +91,13 @@ int** FileHandler::openTxtFile(std::string filename) {
 	bool asSize = true;
 	bool asLine = false;
 
+	// Tweedimensionale array
 	int** graph = new int*[12];
 	for (int i = 0; i < 12; ++i) {
 		graph[i] = new int[12];
 	}
 
-	// Maakt alles 0.
+	// Maakt alles in de tweedimensionale array nul.
 	for (int row = 0; row < 12; row++) {
 		for (int column = 0; column < 12; column++) {
 			graph[row][column] = 0;
@@ -114,7 +115,7 @@ int** FileHandler::openTxtFile(std::string filename) {
 				asSize = false;
 			}
 
-			// 
+			// Haalt de graph op en schrijft dit weg naar een tweedimensionale array.
 			if (asLine == true) {
 
 				int r = INT_MAX;
@@ -123,29 +124,35 @@ int** FileHandler::openTxtFile(std::string filename) {
 				bool asWeight = false;
 				bool asReset = false;
 
+					// Splitst de string eerst op met het sluisteken, vervolgens wordt de uitkomst ook weer gesplitst met komma's.
 					for (int row = 0; row < split(line, '|').size(); row++) {
 						std::string tmp1 = split(line, '|').at(row).c_str();
 						for (int i = 0; i < split(tmp1, ',').size(); i++) {
 							std::string tmp = split(tmp1, ',').at(i).c_str();
 
+							// Stap 3: Daarna wordt de weight op de juiste positie in de graph toegewezen.
 							if (asWeight == true) {
 								weight = stoi(tmp);
+								// Zorgt dat de weight op de juiste positie in de graph wordt toegewezen.
 								graph[r][c] = stoi(tmp);
 								graph[c][r] = stoi(tmp);
 								asWeight = false;
 								asReset = true;
 							}
 
+							// Stap 1: Eerst wordt de row aan toegewezen r.
 							if (r == INT_MAX) {
 								r = stoi(tmp);
 							}
 							else {
+								// Stap 2: Vervolgens wordt de column toegewezen c en wordt asWeight op true gezet.
 								if (c == INT_MAX) {
 									c = stoi(tmp);
 									asWeight = true;
 								}
 							}
 
+							// Stap 4: INT_MAX wordt voor de row, column en weight opnieuw toegewezen.
 							if (asReset == true) {
 								r = INT_MAX; c = INT_MAX; weight = INT_MAX;
 								asReset = false;
@@ -154,9 +161,9 @@ int** FileHandler::openTxtFile(std::string filename) {
 
 					}
 
+					// Stap 5: Verhoogt de column als de volledige row aan de graph is toegewezen.
 					if (column < size) {
 						column++;
-						r = INT_MAX; c = INT_MAX; weight = INT_MAX;
 					}
 					else {
 						column = 0;
