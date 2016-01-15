3c650827e30387778c7419649acf83675001b59d
PCVinnie <vastout@vasnet.nl>
2016-01-15 08:35:00 +0100

    Added more comments

--- DijkstraAlgoritme/DijkstraAlgoritme.cpp
+++ DijkstraAlgoritme/DijkstraAlgoritme.cpp
@@ -237,45 +237,86 @@ void DijkstraAlgoritme::getShortestPathPriorityQueue(std::vector<std::pair<int, 
 	// Print de graph in terminal.
 	printPriorityQueueInput(list);
 
-
+	// Stap 1: Priority_queue wordt aangemaakt. De priority_queue sorteert op de kleinste waarde.
 	std::priority_queue<Weight, std::vector<Weight>, smaller<Weight>> queues;
 
+	// Stap 2: Er worden twee array's aangemaakt. VRTCS staat voor de aantal vertices.
+	// cost[VRTCS] voor het toevoegen van de vertices.
+	// parent[VRTCS] voor het toevoegen van de uitkomst voor de minimale afstand.
 	int cost[VRTCS];
 	int parent[VRTCS];
 
 	int u, v, w;
+	int count = 0;
 
+	// Stap 3: Op basis van de grootte van de constante VRTCS, wordt aan elk element een INT_MAX toegewezen.
+	// INT_MAX is de maximale integer waarden.
 	for (int i = 0; i < VRTCS; i++) {
 		cost[i] = INT_MAX;
 		parent[i] = INT_MAX;
 	}
 
+	// Stap 4: 0 en -1 wordt d.m.v. de start waarden s aan cost[s] en parent[s] toegewezen.
 	cost[s] = 0;
 	parent[s] = -1;
 
+	// Voegt een weight toe aan de priority_queue.
 	queues.push(Weight(s, cost[s]));
 
+	/* 
+		De opzet van stap 1 t/m 4 ziet er dus als volgt uit.
+	
+		cost[s]		parent[s]		priority_queue<>
+		0			-1				Weight(0, cost[0])		
+		INT_MAX		INT_MAX
+		INT_MAX		INT_MAX
+		INT_MAX		INT_MAX
+		INT_MAX		INT_MAX
+		INT_MAX		INT_MAX
+		INT_MAX		INT_MAX
+		INT_MAX		INT_MAX
+		INT_MAX		INT_MAX
+	*/
+
+	/////////////////////////////////////////////////////////////////
+
+	// Zolang de priority_queue niet leeg is blijft het loepen.
 	while (!queues.empty()) {
 
+		count++;
+		std::cout << "--------------------------------------------------------------" << std::endl;
+		std::cout << "Nr.: " << count << std::endl;
+		// Haalt de minimale afstand op uit de priority_queue en wijst dit toe aan u.
 		u = queues.top().u;
+		std::cout << "Stap 1: De minimale afstand is: " << u << std::endl;
+
+		// Verwijdert het eerste element uit de priority_queue.
 		queues.pop();
 
 		for (int i = 0; i < list[u].size(); i++) {
 
-			v = list[u][i].first;
-			w = list[u][i].second;
+			// Haalt de vertex en weight op uit de list[u][i].
+			v = list[u][i].first; // 0
+			w = list[u][i].second; // 4
 
+			// Controleert of cost[v] groter is dan cost[u] opgeteld met de weight uit list[u][i].second.
 			if (cost[v] > cost[u] + w) {
-
+				std::cout << "Stap 2: Als vertex " << v << " groter is dan " << u + w << "(minimale afstand + weight)" << std::endl;
+				// Telt cost[u] en de weight met elkaar op en voegt dit toe aan cost[v].
 				queues.push(Weight(v, cost[v] = cost[u] + w));
+				std::cout << "Stap 3: Telt de minimale afstand en weight met elkaar op, en voegt dit toe aan cost op index nr.: " << v << std::endl;
+				// Voegt de minimale afstand toe aan de parent.
 				parent[v] = u;
-
+				std::cout << "Stap 4: Voegt de minimale afstand " << u << " aan de parent op index nr.: " << v << std::endl;
 			}
 
 		}
 
 	}
 
+	std::cout << "" << std::endl;
+
+	// Print de uitkomst in terminal.
 	printOutput(cost, parent);
 
 }
@@ -301,6 +342,8 @@ void DijkstraAlgoritme::getShortestPathGraph(int** graph, int start, int end) {
 	int parent[VRTCS];
 	bool spt[VRTCS];
 
+	int count = 0;
+
 	// Stap 3: Op basis van de grootte van de constante VRTCS, wordt aan elk element een INT_MAX aan toegewezen.
 	// INT_MAX is de maximale integer waarden.
 	for (int i = 0; i < VRTCS; i++) {
@@ -313,11 +356,32 @@ void DijkstraAlgoritme::getShortestPathGraph(int** graph, int start, int end) {
 	cost[start] = 0;
 	parent[start] = -1;
 
-	// 
+	/*
+		De opzet van stap 1 t/m 4 ziet er dus als volgt uit.
+
+		cost[s]		parent[s]		spt[i]
+		0			-1				false
+		INT_MAX		INT_MAX			false
+		INT_MAX		INT_MAX			false
+		INT_MAX		INT_MAX			false
+		INT_MAX		INT_MAX			false
+		INT_MAX		INT_MAX			false
+		INT_MAX		INT_MAX			false
+		INT_MAX		INT_MAX			false
+		INT_MAX		INT_MAX			false
+	*/
+
+	/////////////////////////////////////////////////////////////////
+
 	for (int i = 0; i < LOOP - 1; i++) {
 
-		// Stap 5: Berekent de minimale afstand.
+		count++;
+		std::cout << "--------------------------------------------------------------" << std::endl;
+		std::cout << "Nr.: " << count << std::endl;
+
+		// Stap 5: Berekent de minimale afstand en wijst de indexnummer toe aan u.
 		int u = minDistance(cost, spt);
+		std::cout << "Stap 1: De minimale afstand is: " << u << std::endl;
 
 		// Markeert de vertex waarmee de minimale afstand is berekend.
 		spt[u] = true;
@@ -333,12 +397,13 @@ void DijkstraAlgoritme::getShortestPathGraph(int** graph, int start, int end) {
 
 					// Controleert of cost[v] groter is dan cost[u] opgeteld met de weight uit de graph.
 					if (cost[v] > cost[u] + g[u][v]) {
-
+						std::cout << "Stap 2: Als vertex " << v << " groter is dan " << cost[u] + g[u][v] << "(minimale afstand + weight)" << std::endl;
 						// Stap 7: Telt cost[u] en de weight uit de graph met elkaar op en voegt dit toe aan cost[v]
 						cost[v] = cost[u] + g[u][v];
+						std::cout << "Stap 3: Telt de minimale afstand en weight met elkaar op, en voegt dit toe aan cost op index nr.: " << v << std::endl;
 						// Voegt de minimale afstand toe aan de parent
 						parent[v] = u;
-
+						std::cout << "Stap 4: Voegt de minimale afstand " << u << " aan de parent op index nr.: " << v << std::endl;
 					}
 
 					// Onderbreekt de for loop als de eind vertex is bereikt.
@@ -352,6 +417,8 @@ void DijkstraAlgoritme::getShortestPathGraph(int** graph, int start, int end) {
 
 		}
 	}
+
+	std::cout << "" << std::endl;
 
 	// Als de eind waarde gelijk is aan 0 dan wordt enkel de printOutput methode aangeroepen.
 	if (end == 0) {
@@ -386,6 +453,8 @@ void DijkstraAlgoritme::getAlternativeShortestPathGraph(int** graph, int s) {
 	int parent[VRTCS];
 	int cost[VRTCS];
 
+	int count = 0;
+
 	// Stap 3: Op basis van de grootte van de constante VRTCS, wordt aan elk element een INT_MAX aan toegewezen.
 	// INT_MAX is de maximale integer waarden.
 	for (int i = 0; i < VRTCS; i++) {
@@ -398,9 +467,30 @@ void DijkstraAlgoritme::getAlternativeShortestPathGraph(int** graph, int s) {
 	parent[s] = -1;
 	cost[s] = 0;
 
+	/*
+		De opzet van stap 1 t/m 4 ziet er dus als volgt uit.
+
+		cost[s]		parent[s]		vector<int> T
+		0			-1				0
+		INT_MAX		INT_MAX			
+		INT_MAX		INT_MAX			
+		INT_MAX		INT_MAX			
+		INT_MAX		INT_MAX			
+		INT_MAX		INT_MAX			
+		INT_MAX		INT_MAX			
+		INT_MAX		INT_MAX			
+		INT_MAX		INT_MAX			
+	*/
+
+	/////////////////////////////////////////////////////////////////
+
 	// Zolang T kleiner is dan de constante VRTCS blijft de while lus lopen.
 	while (T.size() < VRTCS)
 	{
+		count++;
+		std::cout << "--------------------------------------------------------------" << std::endl;
+		std::cout << "Nr.: " << count << std::endl;
+
 		// Initialiseert v met -1 en geeft sc de maximale integer waarde.
 		int v = -1;
 		int sc = INT_MAX;
@@ -409,6 +499,7 @@ void DijkstraAlgoritme::getAlternativeShortestPathGraph(int** graph, int s) {
 		for (int i = 0; i < T.size(); i++)
 		{
 			int u = T[i];
+			std::cout << "Stap 1: De minimale afstand is: " << u << std::endl;
 
 			// Als de priorityqueue niet leeg is en T bevat de zelfde waarde als het hoogste object in de queue.
 			// Dan wordt het hoogste object dat in de priorityqueue staat verwijderd.
@@ -425,19 +516,23 @@ void DijkstraAlgoritme::getAlternativeShortestPathGraph(int** graph, int s) {
 			Weight e = queues[u].top();
 
 			// Stap 6: Telt cost[u] en de weight met elkaar op en controleert of het kleiner is dan sc.
-			if (cost[u] + e.weight < sc)
-			{
+			if (cost[u] + e.weight < sc) {
+				std::cout << "Stap 2: Als vertex " << i << " groter is dan " << cost[u] + e.weight << "(minimale afstand + weight)" << std::endl;
 				v = e.v;
 				// Telt cost[u] en de weight met elkaar op en voegt dit toe aan sc.
 				sc = cost[u] + e.weight;
+				std::cout << "Stap 3: Telt de minimale afstand en weight met elkaar op, en voegt dit toe aan sc: " << sc << std::endl;
 				// Voegt de minimale afstand toe aan de parent.
 				parent[v] = u;
+				std::cout << "Stap 4: Voegt de minimale afstand " << u << " aan de parent op index nr.: " << v << std::endl;
 			}
 		}
 
 		cost[v] = sc;
 		T.push_back(v);
 	}
+
+	std::cout << "" << std::endl;
 
 	// Print de uitkomst in terminal.
 	printOutput(cost, parent);
